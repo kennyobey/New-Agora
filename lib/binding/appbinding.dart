5164227816ba8a4624a34bec6fd0108d916933f3
Adeyeye Adedamola <damscozyblog@gmail.com>
2022-08-28 20:25:32 +0100

    updated code for ios

---
+++ lib/binding/appbinding.dart
@@ -0,0 +1,9 @@
+import 'package:agora_care/services/auth_controller.dart';
+import 'package:get/get.dart';
+
+class AppBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(AuthController(), permanent: true);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
-import 'package:agora_care/app/authentication/login_page.dart';
 import 'package:agora_care/app/onboarding/splashscreen.dart';
+import 'package:agora_care/binding/appbinding.dart';
 import 'package:agora_care/core/constant/app_palette.dart';
 import 'package:agora_care/core/constant/colors.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-import 'services/auth_controller.dart';
 import 'routes/app_router.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding();
-  await Firebase.initializeApp().then(((value) => Get.put(AuthController())));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     DevicePreview(
       enabled: true,
@@ -23,6 +27,7 @@ Future<void> main() async {
 
 // void main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
 //   await SystemChrome.setPreferredOrientations([
 //     DeviceOrientation.portraitUp,
 //     DeviceOrientation.portraitDown,
@@ -46,8 +51,9 @@ class AgoraCare extends StatelessWidget {
       },
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        // initialBinding: AppBinding(),
+        initialBinding: AppBinding(),
         title: 'Agora Care Mobile App',
+        darkTheme: Get.isDarkMode ? ThemeData.dark() : ThemeData.light(),
         theme: ThemeData(
           fontFamily: 'HK GROTESK',
           primarySwatch: Palette.primaryColor,
